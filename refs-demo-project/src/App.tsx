e17697a0b6342dc0f087db843d6a5e53766eebe1
rhtjddls123 <jjjk0605@naver.com>
2025-02-17 18:44:12 +0900

    style: NewProject 컴포넌트 스타일링

--- refs-demo-project/src/App.tsx
+++ refs-demo-project/src/App.tsx
@@ -1,12 +1,13 @@
 import "./App.css";
-import NoProjectSelected from "./components/NoProjectSelected";
+import NewProject from "./components/NewProject";
 import SideBar from "./components/SideBar";
 
 function App() {
   return (
     <main className="my-8 h-screen flex gap-8">
       <SideBar />
-      <NoProjectSelected />
+      {/* <NoProjectSelected /> */}
+      <NewProject />
     </main>
   );
 }
---
+++ refs-demo-project/src/components/NewProject.tsx
@@ -0,0 +1,19 @@
+import InputLabel from "./InputLabel";
+
+const NewProject = () => {
+  return (
+    <div className="w-[35rem] mt-16">
+      <menu className="flex justify-end gap-4 my-4">
+        <button className="text-stone-800 hover:text-stone-950">Cancel</button>
+        <button className="px-6 py-2 bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button>
+      </menu>
+      <div>
+        <InputLabel title="Title" type="text" />
+        <InputLabel title="Description" InputType={"textarea"} />
+        <InputLabel title="Due Date" type="date" />
+      </div>
+    </div>
+  );
+};
+
+export default NewProject;
