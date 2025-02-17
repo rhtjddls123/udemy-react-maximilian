35889454e7b6f6b95d64e4dbf75e26462938f46a
rhtjddls123 <jjjk0605@naver.com>
2025-02-17 17:45:57 +0900

    style: 사이드바 스타일링

--- refs-demo-project/src/App.tsx
+++ refs-demo-project/src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import SideBar from "./components/SideBar";
 
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="w-full my-8 bg-green-500 h-screen flex gap-8">
+      <SideBar />
+    </main>
   );
 }
 
---
+++ refs-demo-project/src/components/SideBar.tsx
@@ -0,0 +1,21 @@
+const SideBar = () => {
+  return (
+    <aside className="bg-stone-900 w-1/3 py-16 px-8 flex flex-col rounded-r-xl">
+      <h2 className="text-stone-200 font-bold text-xl mb-8 uppercase">YOUR PROJECTS</h2>
+      <div>
+        <button className="bg-stone-700 py-2 px-4 rounded-md text-stone-400 hover:bg-stone-600 hover:text-stone-100">
+          + Add Project
+        </button>
+      </div>
+      <ul className="mt-8">
+        <li>
+          <button className="bg-stone-800 text-stone-200 w-full text-left my-1 px-2 py-1 rounded-sm">
+            sad
+          </button>
+        </li>
+      </ul>
+    </aside>
+  );
+};
+
+export default SideBar;
