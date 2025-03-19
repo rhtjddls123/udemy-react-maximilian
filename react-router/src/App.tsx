4c18f6bbd7602581db869b00690b116017428065
rhtjddls123 <jjjk0605@naver.com>
2025-03-20 00:47:03 +0900

    react router 실습

--- react-router/src/App.tsx
+++ react-router/src/App.tsx
@@ -1,5 +1,25 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProductsPage from "./pages/ProductsPage";
+import RootLayout from "./pages/RootLayout";
+import ErrorPage from "./pages/ErrorPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
+      { path: "/products/:productId", element: <ProductDetailPage /> }
+    ]
+  }
+]);
+
 function App() {
-  return <></>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
